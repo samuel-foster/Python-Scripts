0100a2e32b28490e959d677c4e1fa0da21a44cdd
Sam Foster <samuel.foster@outlook.com>
2023-05-25 00:53:11 +0100

    Added: tweet_deleter script

---
+++ Python-Scripts/tweet_deleter.py
@@ -0,0 +1,33 @@
+import tweepy
+
+consumer_key = '3EM7bTB27CxrFPbKdRWx1KU8Z'
+consumer_secret = '8dKNyrBSMXOhlrGs6Paf1tYo6DzGxKkgZ3ulacD8N1ZwvRGns9'
+access_token = '137097621-QIoS4i5jZHef9s4BKRTrEXoufBI3Ak5ST1Wcs9Uh'
+access_token_secret = '01YsIAGW4cJLVB5pk0CDSIP2ON1ekjVzBi2vO4Lsjlrww'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+deleted_count = 0  # Counter for deleted tweets
+
+while True:
+    tweets = api.user_timeline(count=200)  # Retrieve up to 200 of your most recent tweets
+
+    if not tweets:  # No more tweets to delete
+        break
+
+    for tweet in tweets:
+
+        # Optionally, you can also print other information such as the tweet ID, creation time, etc.
+        # Example:
+        # print(f"ID: {tweet.id}")
+        # print(f"Created at: {tweet.created_at}")
+
+        # Uncomment the above lines if you want to print additional information
+
+        api.destroy_status(tweet.id)  # Delete the tweet
+        deleted_count += 1
+
+print(f"Deleted {deleted_count} tweets.")
