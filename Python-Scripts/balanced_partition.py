9b74a6f588bf860204d07f1e0a52ece24259b67b
Sam Foster <samuel.foster@outlook.com>
2023-07-14 16:51:11 +0100

    added: comments

--- Python-Scripts/balanced_partition.py
+++ Python-Scripts/balanced_partition.py
@@ -2,25 +2,25 @@ import math
 from collections import defaultdict
 
 def balanced_partition(table, num_groups):
-    # Step 2
+    # Step 2: Calculate the total number of pods and nodes in the table
     total_pods = len(table)
     total_nodes = len(set(node for _, nodes in table for node in nodes))
 
-    # Step 3
+    # Step 3: Adjust the number of groups if there are fewer nodes than the specified number of groups
     if total_nodes < num_groups:
         num_groups = total_nodes
 
-    # Step 4
+    # Step 4: Calculate the target number of nodes per group
     target_nodes_per_group = math.ceil(total_nodes / num_groups)
 
-    # Step 5
+    # Step 5: Initialize empty lists for each group and a dictionary to store the count of nodes in each group
     groups = [[] for _ in range(num_groups)]
     node_counts = defaultdict(int)
 
-    # Step 6
+    # Step 6: Sort the table based on the number of nodes in each row
     sorted_table = sorted(table, key=lambda row: len(row[1]))
 
-    # Steps 7-9
+    # Steps 7-9: Assign pods to groups while maintaining balance
     for pod, nodes in sorted_table:
         assigned_group = None
         for i in range(num_groups):
@@ -34,7 +34,7 @@ def balanced_partition(table, num_groups):
             groups[min_group].append((pod, nodes))
             node_counts[min_group] += 1
 
-    # Step 10: Add summary of each group
+    # Step 10: Generate a summary of each group
     group_summary = []
     for i, group in enumerate(groups):
         group_summary.append({
@@ -48,9 +48,8 @@ def balanced_partition(table, num_groups):
 
     return groups, group_summary
 
-#Insert table here
-table = []
-
+# Example usage
+table = []  # Insert the table of pods and assigned nodes here
 
 num_groups = 3
 groups, group_summary = balanced_partition(table, num_groups)
