eb55b279de95bb76b8bec0428f7c893ac4222241
Sam Foster <samuel.foster@outlook.com>
2023-07-14 13:53:05 +0100

    fixed: table output formatting

--- Python-Scripts/table_generator.py
+++ Python-Scripts/table_generator.py
@@ -8,22 +8,15 @@ def generate_pod_table(pod_list):
         else:
             pod_table[node_name] = [pod_name]
 
-    return pod_table
+    table = [(node_name, pod_list) for node_name, pod_list in pod_table.items()]
+    return table
 
 # Example usage
 input_list = [
-    "Pod1 Node1",
-    "Pod2 Node2",
-    "Pod3 Node1",
-    "Pod4 Node3",
-    "Pod5 Node2",
-    "Pod6 Node3"
 ]
 
 table = generate_pod_table(input_list)
 
 # Printing the generated table
-for node_name, pod_list in table.items():
-    print("Node:", node_name)
-    print("Pods:", ", ".join(pod_list))
-    print()
+for node_name, pod_list in table:
+    print(f'("{node_name}", {pod_list}),')
